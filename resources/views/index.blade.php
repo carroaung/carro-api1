c1dd31950f803bc3910128327b7fbca9115bb47b
Aung Bo Naing <aung.bn@carro.co>
2022-02-09 10:38:01 +0800

    added carro services added split script

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,5 @@
 <h1>Welcome From Carro API1</h1>
 <hr/>
 <h2>Core function version: {{Carro\Core\Models\Appointment::VERSION}}</h2>
+
+<h2>Service message: {{Carro\Service\HelloworldService::MSG}}</h2>
